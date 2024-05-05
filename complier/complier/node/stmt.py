e9c33b449e4d372c2cfd1e3ea2740b7628897d1d
YyyHc404 <l2739606014@163.com>
2024-05-06 00:15:02 +0800

    2024.5.6  词法分析

---
+++ complier/complier/node/stmt.py
@@ -0,0 +1,17 @@
+from node.node import * 
+class Stmt(Node):
+    pass
+
+class While(Stmt):
+    def __init__(self,) -> None:
+        super().__init__()
+    pass
+class If(Stmt):
+    pass
+
+class Eval(Stmt):
+    pass
+
+class Do(Stmt):
+    pass
+
